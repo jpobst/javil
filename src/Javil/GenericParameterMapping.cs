ceba5cf1c8858330b0e39986705905b019f72e59
Jonathan Pobst <jonathan.pobst@microsoft.com>
2023-07-05 20:32:28 -0500

    Create GenericParameterMapping.GetMappedReference.

--- src/Javil/GenericParameterMapping.cs
+++ src/Javil/GenericParameterMapping.cs
@@ -1,11 +1,12 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace Javil;
+﻿namespace Javil;
 
 public class GenericParameterMapping
 {
     private Dictionary<string, string> _mapping = new Dictionary<string, string> ();
 
+    /// <summary>
+    /// Add a generic parameter mapping, like "T" -> "java.lang.Object"
+    /// </summary>
     public void AddMapping (string name, string value)
     {
         if (name == value)
@@ -14,12 +15,51 @@ public class GenericParameterMapping
         _mapping[name] = value;
     }
 
+    /// <summary>
+    /// Adds a generic paramter mapping from a TypeReference.
+    /// This is from a GenericInstanceType, or from a TypeReference containing one.
+    /// </summary>
     public void AddMappingFromTypeReference (TypeReference typeReference)
     {
         if (typeReference is GenericInstanceType generic_type && typeReference.Resolve () is TypeDefinition resolved_type) {
             for (var i = 0; i < generic_type.GenericArguments.Count; i++)
                 AddMapping (resolved_type.GenericParameters[i].Name, generic_type.GenericArguments[i].FullName);
         }
+    }
+
+    /// <summary>
+    /// Creates a clone of the GenericParameterMapping
+    /// </summary>
+    public GenericParameterMapping Clone () => new GenericParameterMapping { _mapping = _mapping };
+
+    /// <summary>
+    /// Applies the generic parameter mapping to a given TypeReference,
+    /// like converting a java.lang.List<T> to java.lang.List<java.lang.Object>
+    /// </summary>
+    public TypeReference GetMappedReference (TypeReference typeReference)
+    {
+        if (typeReference is GenericParameter gp) {
+            var mapped = GetMapping (gp.Name);
+
+            if (typeReference.Container.FindType (mapped) is TypeDefinition td)
+                return td;
+
+            if (mapped != gp.Name)
+                return new GenericParameter (mapped, typeReference.Container, typeReference.DeclaringType);
+
+            return gp;
+        }
+
+        if (typeReference is GenericInstanceType gt) {
+            var mapped = new GenericInstanceType (gt.Namespace, gt.Name, gt.Container, gt.DeclaringType);
+
+            foreach (var ga in gt.GenericArguments)
+                mapped.GenericArguments.Add (GetMappedReference (ga));
+
+            return mapped;
+        }
+
+        return typeReference;
     }
 
     public string GetMapping (TypeReference typeReference)
